ab7a7c2ba811035d5f80fa259a55f7670c35d129
JuD <vavafs136@gmail.com>
2021-09-06 02:03:43 +0900

    first

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const weather = document.querySelector('#weather span:first-child');
+const city = document.querySelector('#weather span:last-child');
+const api_key = '4eef06f09a6ff8a2fc3ab50795f00d27';
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
